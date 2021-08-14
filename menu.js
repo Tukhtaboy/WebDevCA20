756df86eb1720154864df28fcbbac19c2ecb3a33
Tukhtaboy <69669525+Tukhtaboy@users.noreply.github.com>
2021-08-14 15:41:55 +0300

    Menu JS
    Menu JS

---
+++ menu.js
@@ -0,0 +1,313 @@
+    var iStar1 = document.getElementById('iStar1').style.color;
+    var iStar2 = document.getElementById('iStar2').style.color;
+    var iStar3 = document.getElementById('iStar3').style.color;
+    var iStar4 = document.getElementById('iStar4').style.color;
+    var iStar5 = document.getElementById('iStar5').style.color;
+function ckeckStar1() {
+    if(iStar1 == ""){
+        document.getElementById('iStar1').style.color = "yellow";
+        document.getElementById('message').innerHTML = "1.0"   
+    }
+    if(document.getElementById('message').innerHTML == "1.0"){
+        // document.getElementById('iStar1').style.color = "yellow";
+        document.getElementById('iStar2').style.color = "black";
+        document.getElementById('iStar3').style.color = "black";
+        document.getElementById('iStar4').style.color = "black";
+        document.getElementById('iStar5').style.color = "black";
+    }
+}
+function ckeckStar2() {
+    if(iStar2 == ""){
+        document.getElementById('iStar1').style.color = "yellow";
+        document.getElementById('iStar2').style.color = "yellow";
+        document.getElementById('message').innerHTML = "2.0"   
+
+
+    }
+    if(document.getElementById('message').innerHTML == "2.0" ){
+        document.getElementById('iStar3').style.color = "black";
+        document.getElementById('iStar4').style.color = "black";
+        document.getElementById('iStar5').style.color = "black";
+    }
+}
+function ckeckStar3() {
+    if(iStar3 == ""){
+        document.getElementById('iStar1').style.color = "yellow";
+        document.getElementById('iStar2').style.color = "yellow";
+        document.getElementById('iStar3').style.color = "yellow";
+        document.getElementById('message').innerHTML = "3.0"   
+
+
+    }
+    if(document.getElementById('message').innerHTML = "3.0"){
+        document.getElementById('iStar4').style.color = "black";
+        document.getElementById('iStar5').style.color = "black";
+    }
+}
+function ckeckStar4() {
+    if(iStar4 == ""){
+        document.getElementById('iStar1').style.color = "yellow";
+        document.getElementById('iStar2').style.color = "yellow";
+        document.getElementById('iStar3').style.color = "yellow";
+        document.getElementById('iStar4').style.color = "yellow";
+        document.getElementById('message').innerHTML = "4.0"   
+
+
+    }
+    if(document.getElementById('message').innerHTML = "4.0"){
+        document.getElementById('iStar5').style.color = "black";
+    }
+}
+function ckeckStar5() {
+    if(iStar5 == ""){
+        document.getElementById('iStar1').style.color = "yellow";
+        document.getElementById('iStar2').style.color = "yellow";
+        document.getElementById('iStar3').style.color = "yellow";
+        document.getElementById('iStar4').style.color = "yellow";
+        document.getElementById('iStar5').style.color = "yellow";
+        document.getElementById('message').innerHTML = "5.0"   
+
+
+    }
+    // if(iStar2 == ""){
+    //     document.getElementById('iStar2').style.color = "red";
+    // }
+}
+
+
+
+/*    222222  */
+var iStar21 = document.getElementById('iStar21').style.color;
+var iStar22 = document.getElementById('iStar22').style.color;
+var iStar23 = document.getElementById('iStar23').style.color;
+var iStar24 = document.getElementById('iStar24').style.color;
+var iStar25 = document.getElementById('iStar25').style.color;
+function ckeckStar21() {
+if(iStar21 == ""){
+    document.getElementById('iStar21').style.color = "yellow";
+    document.getElementById('message2').innerHTML = "1.0"   
+}
+if(document.getElementById('message2').innerHTML == "1.0"){
+    // document.getElementById('iStar1').style.color = "yellow";
+    document.getElementById('iStar22').style.color = "black";
+    document.getElementById('iStar23').style.color = "black";
+    document.getElementById('iStar24').style.color = "black";
+    document.getElementById('iStar25').style.color = "black";
+}
+}
+function ckeckStar22() {
+if(iStar22 == ""){
+    document.getElementById('iStar21').style.color = "yellow";
+    document.getElementById('iStar22').style.color = "yellow";
+    document.getElementById('message2').innerHTML = "2.0"   
+
+
+}
+if(document.getElementById('message2').innerHTML == "2.0" ){
+    document.getElementById('iStar23').style.color = "black";
+    document.getElementById('iStar24').style.color = "black";
+    document.getElementById('iStar25').style.color = "black";
+}
+}
+function ckeckStar23() {
+if(iStar23 == ""){
+    document.getElementById('iStar21').style.color = "yellow";
+    document.getElementById('iStar22').style.color = "yellow";
+    document.getElementById('iStar23').style.color = "yellow";
+    document.getElementById('message2').innerHTML = "3.0"   
+
+
+}
+if(document.getElementById('message2').innerHTML = "3.0"){
+    document.getElementById('iStar24').style.color = "black";
+    document.getElementById('iStar25').style.color = "black";
+}
+}
+function ckeckStar24() {
+if(iStar24 == ""){
+    document.getElementById('iStar21').style.color = "yellow";
+    document.getElementById('iStar22').style.color = "yellow";
+    document.getElementById('iStar23').style.color = "yellow";
+    document.getElementById('iStar24').style.color = "yellow";
+    document.getElementById('message2').innerHTML = "4.0"   
+
+
+}
+if(document.getElementById('message2').innerHTML = "4.0"){
+    document.getElementById('iStar25').style.color = "black";
+}
+}
+function ckeckStar25() {
+if(iStar25 == ""){
+    document.getElementById('iStar21').style.color = "yellow";
+    document.getElementById('iStar22').style.color = "yellow";
+    document.getElementById('iStar23').style.color = "yellow";
+    document.getElementById('iStar24').style.color = "yellow";
+    document.getElementById('iStar25').style.color = "yellow";
+    document.getElementById('message2').innerHTML = "5.0"   
+
+
+}
+// if(iStar2 == ""){
+//     document.getElementById('iStar2').style.color = "red";
+// }
+}
+
+
+
+
+
+/*    333333  */
+var iStar31 = document.getElementById('iStar31').style.color;
+var iStar32 = document.getElementById('iStar32').style.color;
+var iStar33 = document.getElementById('iStar33').style.color;
+var iStar34 = document.getElementById('iStar34').style.color;
+var iStar35 = document.getElementById('iStar35').style.color;
+function ckeckStar31() {
+if(iStar31 == ""){
+    document.getElementById('iStar31').style.color = "yellow";
+    document.getElementById('message3').innerHTML = "1.0"   
+}
+if(document.getElementById('message3').innerHTML == "1.0"){
+    // document.getElementById('iStar1').style.color = "yellow";
+    document.getElementById('iStar32').style.color = "black";
+    document.getElementById('iStar33').style.color = "black";
+    document.getElementById('iStar34').style.color = "black";
+    document.getElementById('iStar35').style.color = "black";
+}
+}
+function ckeckStar32() {
+if(iStar32 == ""){
+    document.getElementById('iStar31').style.color = "yellow";
+    document.getElementById('iStar32').style.color = "yellow";
+    document.getElementById('message3').innerHTML = "2.0"   
+
+
+}
+if(document.getElementById('message3').innerHTML == "2.0" ){
+    document.getElementById('iStar33').style.color = "black";
+    document.getElementById('iStar34').style.color = "black";
+    document.getElementById('iStar35').style.color = "black";
+}
+}
+function ckeckStar33() {
+if(iStar33 == ""){
+    document.getElementById('iStar31').style.color = "yellow";
+    document.getElementById('iStar32').style.color = "yellow";
+    document.getElementById('iStar33').style.color = "yellow";
+    document.getElementById('message3').innerHTML = "3.0"   
+
+
+}
+if(document.getElementById('message3').innerHTML = "3.0"){
+    document.getElementById('iStar34').style.color = "black";
+    document.getElementById('iStar35').style.color = "black";
+}
+}
+function ckeckStar34() {
+if(iStar34 == ""){
+    document.getElementById('iStar31').style.color = "yellow";
+    document.getElementById('iStar32').style.color = "yellow";
+    document.getElementById('iStar33').style.color = "yellow";
+    document.getElementById('iStar34').style.color = "yellow";
+    document.getElementById('message3').innerHTML = "4.0"   
+
+
+}
+if(document.getElementById('message3').innerHTML = "4.0"){
+    document.getElementById('iStar35').style.color = "black";
+}
+}
+function ckeckStar35() {
+if(iStar35 == ""){
+    document.getElementById('iStar31').style.color = "yellow";
+    document.getElementById('iStar32').style.color = "yellow";
+    document.getElementById('iStar33').style.color = "yellow";
+    document.getElementById('iStar34').style.color = "yellow";
+    document.getElementById('iStar35').style.color = "yellow";
+    document.getElementById('message3').innerHTML = "5.0"   
+
+
+}
+// if(iStar2 == ""){
+//     document.getElementById('iStar2').style.color = "red";
+// }
+}
+
+
+/*    444444  */
+var iStar41 = document.getElementById('iStar41').style.color;
+var iStar42 = document.getElementById('iStar42').style.color;
+var iStar43 = document.getElementById('iStar43').style.color;
+var iStar44 = document.getElementById('iStar44').style.color;
+var iStar45 = document.getElementById('iStar45').style.color;
+function ckeckStar41() {
+if(iStar41 == ""){
+    document.getElementById('iStar41').style.color = "yellow";
+    document.getElementById('message4').innerHTML = "1.0"   
+}
+if(document.getElementById('message4').innerHTML == "1.0"){
+    // document.getElementById('iStar1').style.color = "yellow";
+    document.getElementById('iStar42').style.color = "black";
+    document.getElementById('iStar43').style.color = "black";
+    document.getElementById('iStar44').style.color = "black";
+    document.getElementById('iStar45').style.color = "black";
+}
+}
+function ckeckStar42() {
+if(iStar42 == ""){
+    document.getElementById('iStar41').style.color = "yellow";
+    document.getElementById('iStar42').style.color = "yellow";
+    document.getElementById('message4').innerHTML = "2.0"   
+
+
+}
+if(document.getElementById('message4').innerHTML == "2.0" ){
+    document.getElementById('iStar43').style.color = "black";
+    document.getElementById('iStar44').style.color = "black";
+    document.getElementById('iStar45').style.color = "black";
+}
+}
+function ckeckStar43() {
+if(iStar43 == ""){
+    document.getElementById('iStar41').style.color = "yellow";
+    document.getElementById('iStar42').style.color = "yellow";
+    document.getElementById('iStar43').style.color = "yellow";
+    document.getElementById('message4').innerHTML = "3.0"   
+
+
+}
+if(document.getElementById('message4').innerHTML = "3.0"){
+    document.getElementById('iStar44').style.color = "black";
+    document.getElementById('iStar45').style.color = "black";
+}
+}
+function ckeckStar44() {
+if(iStar44 == ""){
+    document.getElementById('iStar41').style.color = "yellow";
+    document.getElementById('iStar42').style.color = "yellow";
+    document.getElementById('iStar43').style.color = "yellow";
+    document.getElementById('iStar44').style.color = "yellow";
+    document.getElementById('message4').innerHTML = "4.0"   
+
+
+}
+if(document.getElementById('message3').innerHTML = "4.0"){
+    document.getElementById('iStar35').style.color = "black";
+}
+}
+function ckeckStar45() {
+if(iStar45 == ""){
+    document.getElementById('iStar41').style.color = "yellow";
+    document.getElementById('iStar42').style.color = "yellow";
+    document.getElementById('iStar43').style.color = "yellow";
+    document.getElementById('iStar44').style.color = "yellow";
+    document.getElementById('iStar45').style.color = "yellow";
+    document.getElementById('message4').innerHTML = "5.0"   
+
+
+}
+// if(iStar2 == ""){
+//     document.getElementById('iStar2').style.color = "red";
+// }
+}
