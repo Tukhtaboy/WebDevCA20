4fbfab11e60457c06cff9fedcb4d33bb8ef9ef58
Tukhtaboy <69669525+Tukhtaboy@users.noreply.github.com>
2021-08-14 15:27:53 +0300

    Footer of restaurant website
    Footer

---
+++ footer.php
@@ -0,0 +1,43 @@
+</div>
+<!-- Footer Starts Here -->
+<footer style="background: #000; color:#ffe817;">
+  <div class="container">
+    <div class="row">
+      <address class="col-lg-4">
+        <p>
+          Sushi Heaven <br>
+          Street 123 13916 Helsinki <br>
+          <a href="mailto: stoprich@gmail.com">stoprichn@gmail.com</a> <br>
+          Tel. +3580408170563
+         </p>
+      </address>
+      <div class="col-lg-4">
+        <p>
+          Opening Hours <br>
+          Mon-Fri: 09:00 - 21:00 <br>
+          Sat: 11:00 - 23:00 <br>
+          Sun: Closed
+        </p>
+      </div>
+      <div class="col-lg-4 social-media-icons">
+        <ul>
+          <li><a href="https://www.facebook.com/" target="_blank"><img src="images/facebook-icon.png" alt="Facebook Icon" class="img-fluid"></a></li>
+          <li><a href="https://vk.com/" target="_blank"><img src="images/vk-icon.png" alt="VK Icon" class="img-fluid"></a></li>
+          <li><a href="https://www.instagram.com" target="_blank"><img src="images/instagram-icon.png" alt="Instagram Icon" class="img-fluid"></a></li>
+          <li><a href="https://wolt.com/" target="_blank"><img src="images/wolt-icon.png" alt="Wolt Icon" class="img-fluid"></a></li>
+        </ul>
+          <a href="https://www.tripadvisor.com/" target="_blank"><img src="images/tripadvisor-icon.png" alt="Tripadvisor Icon" class="tripadvisor" class="img-fluid"></a>
+        </div>
+      </div>
+  <div class="row">
+    <div class="col-md-12 copyright">
+      <p>Copyright © Sushi Heaven 2021</p>
+    </div>
+  </div>
+</div>
+</footer>
+<!-- Footer Ends Here -->
+</body>
+</html>
+
+
